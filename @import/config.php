124ad9668c1dbfa738c9a0749b463f8f920aa8dd
Safflower <fuzzer@nate.com>
2018-01-08 03:34:19 +0900

    Update config.php

--- @import/config.php
+++ @import/config.php
@@ -12,11 +12,6 @@
 		header('Location: '.$page);
 		die;
 	}
-	function error($code){
-		$_SERVER['REDIRECT_STATUS'] = $code;
-		require __DIR__.'/../error.php';
-		die;
-	}
 	function is_valid_path($path){
 		return preg_match('/^[a-z0-9_-]+$/s', $path);
 	}
